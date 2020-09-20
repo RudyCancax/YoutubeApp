aeaef662f7ed083079e77b78ca52ba5690559122
rudycancax@gmail.com <rudycancax@gmail.com>
2020-09-19 18:06:55 -0600

    Final

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import { YoutubeService } from '../../services/youtube.service';
+import { YoutubeService } from '../../services/youtube.service';
 import { Video } from '../../models/youtube.models';
 import Swal from 'sweetalert2';
 
@@ -13,12 +13,12 @@ export class HomeComponent implements OnInit {
   // Arreglo tipo Video (Está en los modelos), se agreag para que typescript ayude
   videos: Video[] = [];
 
-  // constructor( private youtubeService: YoutubeService ) { }
-  constructor( ) { }
+  constructor( private youtubeService: YoutubeService ) { }
+  // constructor( ) { }
 
   ngOnInit(): void {
     // Al iniciar el proyecto carga los primeros 20 videos
-    // this.cargarVideos();
+    this.cargarVideos();
   }
 
 
@@ -26,35 +26,35 @@ export class HomeComponent implements OnInit {
 
   // Muestra el video en el Pop Up, usando 'SweetAlert'
 
-  // mostrarVideoPopUp( video: Video ) {
-  //   console.log(video);
+  mostrarVideoPopUp( video: Video ) {
+    console.log(video);
 
-  //   Swal.fire({
-  //     html: `
-  //     <h4>${ video.title} </h4>
-  //     <iframe
-  //         width="100%"
-  //         height="315"
-  //         src="https://www.youtube.com/embed/${ video.resourceId.videoId }"
-  //         frameborder="0"
-  //         allow="accelerometer;
-  //         autoplay;
-  //         encrypted-media;
-  //         gyroscope;
-  //         picture-in-picture"
-  //         allowfullscreen>
-  //     </iframe>`
-  //   });
-  // }
+    Swal.fire({
+      html: `
+      <h4>${ video.title} </h4>
+      <iframe
+          width="100%"
+          height="315"
+          src="https://www.youtube.com/embed/${ video.resourceId.videoId }"
+          frameborder="0"
+          allow="accelerometer;
+          autoplay;
+          encrypted-media;
+          gyroscope;
+          picture-in-picture"
+          allowfullscreen>
+      </iframe>`
+    });
+  }
 
   // Al hacer click en el botón Siguiente Página, carga 20 videos más
 
-  // cargarVideos() {
-  //   this.youtubeService.getVideos()
-  //     .subscribe(respuesta => {
-  //       this.videos.push( ...respuesta );
-  //       console.log(respuesta);
-  //     });
-  // }
+  cargarVideos() {
+    this.youtubeService.getVideos()
+      .subscribe(respuesta => {
+        this.videos.push( ...respuesta );
+        console.log(respuesta);
+      });
+  }
 
 }
